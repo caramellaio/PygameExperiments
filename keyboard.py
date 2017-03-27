04bbec2d21674431a1dc5dd9b99eedeacf8ba3d3
caramellaio <albertobombardelli123@gmail.com>
2017-03-27 21:41:39 +0200

    fix backspace bug

--- keyboard.py
+++ keyboard.py
@@ -7,31 +7,38 @@ class KeyHandler():
         #self.message_event = message_event
         self.text = ""
         self.shift = False
-        self.cursor_pos = -1
+        self.cursor_pos = 0
 
     def handle_key_up(self,event):
         pass
 
     def handle_key_down(self,event):
+
         if event.key in self.special_keys:
             self._on_special_code(event)
         elif event.key == pygame.K_BACKSPACE:
-            self.text = self.text[:self.cursor_pos] + self.text[self.cursor_pos+1:]
-            self.cursor_pos-=1
+            self.text = self.text[:self.cursor_pos-1] + self.text[self.cursor_pos+1:]
+            self.move_cursor_l()
         elif event.key == pygame.K_RIGHT:
-            self.cursor_pos = min(self.cursor_pos+1,len(self.text))
+            self.move_cursor_r()
+
         elif event.key == pygame.K_LEFT:
-            self.cursor_pos = max(0,self.cursor_pos-1)
+            self.move_cursor_l()    
+            
         elif event.key == pygame.KMOD_LSHIFT or event.key ==pygame.KMOD_RSHIFT:
             self.shift = True
         else:
-            print(":")
             self.text = self.text[:self.cursor_pos] + chr(event.key) + self.text[self.cursor_pos:]
-            self.cursor_pos+=1
-            print(chr(event.key))
-    
+            self.move_cursor_r()
+
+        print(self.cursor_pos)
+
     def _on_special_code(self,event):
-        print("log therapy")
+        pass
         #self.message_event.put_msg(event) # I'll probably change this...add_event(event
 
+    def move_cursor_r(self):
+        self.cursor_pos = self.cursor_pos +1 if self.cursor_pos <= len(self.text) else len(self.text)
 
+    def move_cursor_l(self):
+        self.cursor_pos = max(self.cursor_pos -1, 0)
