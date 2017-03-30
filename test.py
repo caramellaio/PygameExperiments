dc9f72b52c960c06c44986b7cf8d53bfff581c72
caramellaio <albertobombardelli123@gmail.com>
2017-03-30 12:31:46 +0200

    removed useless kkk.draw function call

--- test.py
+++ test.py
@@ -46,7 +46,6 @@ def main():
             #screen.blit(background,(random.randint(0,1000),random.randint(0,1000)))
             #text = font.render("sieg heil",1,(10,10,10))
         b.draw(screen)
-        kkk.draw()
         if k_hand.handle_on_clock():
             kkk.draw(k_hand.text,k_hand.cursor_pos)
         pygame.display.update()
